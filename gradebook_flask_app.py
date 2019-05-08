00cf378a945fd1f5c90fa8aad89179406c331815
andrew-austin-2 <49663990+andrew-austin-2@users.noreply.github.com>
2019-05-07 22:31:16 -0400

    mostly functional edit_grades function

--- gradebook_flask_app.py
+++ gradebook_flask_app.py
@@ -63,9 +63,42 @@ def roster():
     return render_template('roster.html')
 
 #andrew
-@app.route('/edit_grades')
+@app.route('/edit_grades', methods=["GET", "POST"])
 def editGrades():
-    return 'Edit grades'
+    if request.method == 'GET':
+        return render_template("edit_grades.html", students=Student.query.all(), grades=Grade.query.all(), assignments=Assignment.query.all())
+    #can probably get rid of the first and last names. user can verify with the table below for the correct student based on the id
+    studID = request.form["studID"]
+    assignmentName = request.form["assignmentName"]
+    gradeReceived = request.form["assignmentGrade"]
+
+    #search to get the assignment ID
+    assignmentID = Assignment.query.filter_by(assignmentName=assignmentName).first()
+    assignmentID = assignmentID.assignmentId
+    #Assignment action must be done first. Check to see if it exists for that student.
+
+    gradeToChange = Grade.query.filter_by(studentId=studID, assignmentId=assignmentID, grade=gradeReceived).first() # checks to see if we need to add a grade or just update.
+
+    if gradeToChange is None:
+        #add/update new grade to grade table
+        updateOrAdd = Grade.query.filter_by(studentId=studID, assignmentId=assignmentID).first() # if it returns something (not None)... then update table, if not (None), add to table
+        changeGrade = Grade(studentId=studID, assignmentId=assignmentID, grade=gradeReceived)
+        if updateOrAdd is None:
+            db.session.add(changeGrade)
+            db.session.commit()
+        else:
+            updateOrAdd.grade = gradeReceived
+            db.session.commit()
+    else:
+        #there was already a grade for that assignment and user with the same grade.
+        gradeToChange = False
+
+
+        return render_template('edit_grades.html', gradeToChange=gradeToChange, students=Student.query.all(), grades=Grade.query.all(), assignments=Assignment.query.all())
+    #assignment = Assignment(assignmentId=assignmentID)
+    #grade = Grade()
+
+    return redirect(url_for('editGrades'))
 
 #andrew
 @app.route("/add_delete_student", methods=["GET", "POST"])
